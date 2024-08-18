fc7c462c822048df23693facda7f29c9bd79b8fc
Shane Freeder <theboyetronic@gmail.com>
2024-08-18 13:37:59 +0100

    marginal improvements to autoplay

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 // https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
+let hasStarted = false;
 window.onscroll = function () {
   const topButton = document.getElementById("top-button");
 
-  console.log(document.documentElement.scrollTop);
   if (document.documentElement.scrollTop > 30) {
     topButton.style.display = "";
   } else {
     topButton.style.display = "none";
   }
 
+  if (hasStarted) return;
   // Google commenced a war with auto playing media and so, this will
   // not play by default. There are seemingly some hacks around this, however
   // I failed to get them to work, so, we'll just do this. This will not work until
   // a client has clicked the page.
   /** @type {HTMLAudioElement} */
   let audioPlayer = document.getElementById("player");
-  audioPlayer.play();
+
+  // https://developer.chrome.com/blog/autoplay/#audiovideo_elements
+  let promise = audioPlayer.play();
+  if (promise !== undefined) {
+    promise
+      .then((_) => {
+        // Autoplay started!
+        audioPlayer.volume = 0.2;
+        hasStarted = true;
+      })
+      .catch((error) => {
+        // Autoplay was prevented.
+        // Show a "Play" button so that user can start playback.
+      });
+  }
 };
